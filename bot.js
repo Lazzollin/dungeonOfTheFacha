2c3eecd2532930ed9ea5e758928677a610cc0e7d
Lazzollin <48962891+Lazzollin@users.noreply.github.com>
2020-09-05 00:20:53 -0300

    Big update

--- bot.js
+++ bot.js
@@ -1,52 +1,103 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+// importing Discord.js
+const Discord = require('discord.js')
+const { BOT_TOKEN } = require('./token')
 
-function game() {
-    p = ":sunglasses:";
-    firstFrame = `\n╬═══════════════════════════════════╬\n║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n║░░░${p}░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n╬═══════════════════════════════════╬`
-
-    
-    var frame = firstFrame;
-
-    return(frame)
-}
+client = new Discord.Client();
 
 client.on('ready' , () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
-
 client.on('message', msg => {
-    if (msg.content === 'juegar') {
-        console.log(msg.user);
-        msg.channel.send(game());
+    if (msg.content === `!!play`) {
+        console.log(msg.content);
+        try {
+            msg.channel.send(buildMap(6,4))
+        } catch(err) {
+            console.log(err)
+        }
     }
 });
 
-client.login('NzUwNDYzNzA5ODc2NTg0NjI5.X065yw.A2cGGifmW5tvckDFo8QhSzb04bQ')
+client.login(BOT_TOKEN)
 
+function player() {
+    const facha = `:sunglasses:`
+    let pos_x = 1
+    let pos_y = 1
+}
 
-function frame() {
-    return(this.$firstFrame)
-};
+function buildMap(col, row) {
+    const top_right_corner = [`╗ `]
+    const top_left_corner = [`╔`]
+    const bottom_right_corner = [`╝`]
+    const bottom_left_corner = [`╚`]
+    const bottom_left_corner2 = [`╚`]
+    const horizontal_frame = [`═`]
+    const inside = [`░`]
+    const vertical_frame = [`║ \n`]
+    const vertical_frame2 = [`\n║`]
+    
+    let map = [`${top_left_corner}`];
+
+    let x = 1
+    let y = 1
+    let positionCount = 0
+
+    for (i=0;i<row;i++) {
+        console.log(`row N° = ${y}`)
+        if (y != 1 && y != row) {
+            map = map.slice(0, -1) + vertical_frame2
+        } else if (y == row) {
+            map = map.slice() + bottom_left_corner2
+        }
+        for (j=0;j<col;j++){
+            console.log(`column N° ${x}`)
+            if (y == 1 || y == row) {
+                map += horizontal_frame
+            } else {
+                map += inside
+            }
+            x++
+        }
+        if (y == 1) {
+            map = map.slice(0, -1) + top_right_corner
+            y++
+        } else if ( y == row) {
+            map = map.slice(0, -1) + bottom_right_corner
+            y++
+        } else {
+            map = map.slice(0, -1) + vertical_frame
+            y++
+        }
+
+        //map += "\n"
+    }
+    console.log(map.length)
+    return map.toString()
+}
+
+String.prototype.replaceAt = function (index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+}
 
 //NzUwNDYzNzA5ODc2NTg0NjI5.X065yw.A2cGGifmW5tvckDFo8QhSzb04bQ
 
-/*var numero = Math.floor(Math.random() * 5);
-    console.log(numero)
-    switch(numero) {
-        case 0:
-            return ("Laza gei");
-            break;
-        case 1:
-            return ("Mateo gei");
-            break;
-        case 2:
-            return ("Tronce gei");
-            break;
-        case 3:
-            return ("Manu gei");
-            break;
-        case 4:
-            return ("Balido gei");
-            break;
-    }*/
+/*firstFrame = `
+    ╔═══════════════════════════════════╗\n
+    ║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n
+    ║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n
+    ║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n
+    ║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n
+    ║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n
+    ║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n
+    ║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n
+    ║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n
+    ║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n
+    ║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n
+    ║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n
+    ║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n
+    ║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n
+    ║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n
+    ║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░║\n
+    ╚═══════════════════════════════════╝`;*/
+
---
+++ token.js
@@ -0,0 +1,3 @@
+module.exports = {
+    BOT_TOKEN: 'NzUwNDYzNzA5ODc2NTg0NjI5.X065yw.4Yjni--bTkai3ouXvKZtL4_pgZU',
+}
